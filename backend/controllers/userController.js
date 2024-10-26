8312acd60f54f65a972fb1e29b1fe41bd057d1f6
visheshtomarr <visheshtomar7800@gmail.com>
2024-10-26 17:12:51 +0530

    Created userModel to register users to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 // Description - Register a new user
 // Route - /api/users
@@ -15,7 +17,38 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("Please include all fields");
     }
 
-    res.send("Register Route");
+    // If the user already exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    // Hash password using bcryptjs.
+    // Create a salt that will be the hashed string.
+    const salt = await bcrypt.genSalt(10);
+    // Create the hashed password using the salt.
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user using hashed password.
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    if (user) {
+        res.status(201).json({
+            // MongoDB syntax to store id.
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
 })
 
 // Description - Login a user
