e3fae3712adf626a2d9f88336685d59f673cf100
visheshtomarr <visheshtomar7800@gmail.com>
2024-11-07 18:19:58 +0530

    Refactored code to register user to db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if (userExists) {
         res.status(400);
-        throw new Error("User already exists");
+        throw new Error("User already exists!");
     }
 
     // Hash password using bcryptjs.
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
-import { register } from "../features/auth/authSlice";
+import { register, reset } from "../features/auth/authSlice";
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -14,11 +15,25 @@ function Register() {
 
     const { name, email, password, password2 } = formData;
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const { user, isLoading, isSuccess, message } = 
+    const { user, isLoading, isError, isSuccess, message } = 
         useSelector(
             (state) => state.auth
         );
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        // Redirect if logged in successfully.
+        if (isSuccess || user) {
+            navigate("/");
+        }
+
+        dispatch(reset());
+    }, [isError, isSuccess, user, message, navigate, dispatch])
 
     const handleChange = (e) => {
         setFormData((prevState) => ({
@@ -47,7 +62,7 @@ function Register() {
         <>
             <section className="heading">
                 <h1>
-                    <FaUser /> Register {user}
+                    <FaUser /> Register
                 </h1>
                 <p>Please create an account</p>
             </section>
