0872fe27ac002a5884e72447b28b2279402d3fb5
visheshtomarr <visheshtomar7800@gmail.com>
2024-11-08 13:09:02 +0530

    Implemented getTickets and createTicket functionality

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -6,14 +6,50 @@ const Ticket = require("../models/ticketModel");
 // Route - GET api/tickets
 // Access - Private
 const getTickets = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "getTickets" });
+    // Get user from id in the JSON Web Token.
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("User not found!");
+    }
+
+    const tickets = await Ticket.find({ user: req.user.id });
+
+    res.status(200).json(tickets);
 })
 
 // Description - Create new ticket
 // Route - POST api/tickets
 // Access - Private
 const createTicket = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "createTicket" });
+    const { product, description } = req.body;
+
+    if(!product || !description) {
+        // Bad request
+        res.status(400)
+        throw new Error("Please add a product and description!");
+    }
+
+    // Get user from id in the JSON Web Token.
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("User not found!");
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: "new"
+    })
+
+    // A new ticket is created successfully, so we return a 201 status.
+    res.status(201).json(ticket);
 })
 
 module.exports = {
