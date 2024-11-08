a54aaf36bb767a71db3ceb1aca673eb8626bddb9
visheshtomarr <visheshtomar7800@gmail.com>
2024-11-08 18:16:59 +0530

    Refactored Tickets page to display user tickets

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getTickets, reset } from "../features/ticket/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 
 function Tickets() {
     const { tickets, isLoading, isSuccess } = useSelector((state) => 
@@ -20,9 +21,21 @@ function Tickets() {
     }
 
     return (
-        <div>
+        <>
+            <BackButton url="/" /> 
             <h1>Tickets</h1>
-        </div>
+            <div className="tickets">
+                <div className="ticket-headings">
+                    <div>Date</div>
+                    <div>Product</div>
+                    <div>Status</div>
+                    <div></div>
+                </div>
+                {tickets.map((ticket) => (
+                    <TicketItem key={ticket._id} ticket={ticket} />
+                ))}
+            </div>
+        </>
     )
 }
 
