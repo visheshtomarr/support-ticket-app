4be1f942fa5c553b8efd62de5e29ad07511a4d6a
visheshtomarr <visheshtomar7800@gmail.com>
2024-11-08 17:16:06 +0530

    Refactored code to create a new ticket and store it in db

--- frontend/src/features/ticket/ticketSlice.js
+++ frontend/src/features/ticket/ticketSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import ticketService from "./ticketService";
 
 const initialState = {
     tickets: [],
@@ -9,13 +10,48 @@ const initialState = {
     message: ""
 }
 
+// Create a new ticket.
+export const createTicket = createAsyncThunk(
+    "ticket/create", 
+    async (ticketData, thunkAPI) => {
+        try {
+            // We need to pass in the token as well since we are now trying to
+            // access a protected route. So, we will use thunkAPI's getState() method
+            // to fetch the user's token from "auth".
+            const token = thunkAPI.getState().auth.user.token;
+            return await ticketService.createTicket(ticketData, token);
+        } catch (error) {
+            const message = (
+                error.response 
+                && error.response.data 
+                && error.response.data.message
+            ) || error.message || error.toString() ;
+
+            return thunkAPI.rejectWithValue(message);
+        }
+})
+
 export const ticketSlice = createSlice({
     name: "ticket",
     initialState,
     reducers: {
         reset: (state) => initialState,
     },
-    extraReducers: (builder) => {}
+    extraReducers: (builder) => {
+        builder
+        .addCase(createTicket.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(createTicket.fulfilled, (state) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+        })
+        .addCase(createTicket.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+        })
+    }
 })
 
 export const { reset } = ticketSlice.actions;
