165440216a106dfa4162b927c2107f1d181eca50
visheshtomarr <visheshtomar7800@gmail.com>
2024-11-08 14:55:49 +0530

    Implemented get/delete/update ticket functionalities

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -20,6 +20,37 @@ const getTickets = asyncHandler(async (req, res) => {
     res.status(200).json(tickets);
 })
 
+// Description - Get user ticket
+// Route - GET api/tickets/:id
+// Access - Private
+const getTicket = asyncHandler(async (req, res) => {
+    // Get user from id in the JSON Web Token.
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("User not found!");
+    }
+
+    const ticket = await Ticket.findById(req.params.id);
+
+    // If ticket with the passed id does not exist.
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found!");
+    }
+
+    // Only the ticket owner can fetch their ticket.
+    if(ticket.user.toString() !== req.user.id) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("Not Authorized!");
+    }
+
+    res.status(200).json(ticket);
+})
+
 // Description - Create new ticket
 // Route - POST api/tickets
 // Access - Private
@@ -52,7 +83,80 @@ const createTicket = asyncHandler(async (req, res) => {
     res.status(201).json(ticket);
 })
 
+// Description - Delete a ticket
+// Route - DELETE api/tickets/:id
+// Access - Private
+const deleteTicket = asyncHandler(async (req, res) => {
+    // Get user from id in the JSON Web Token.
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("User not found!");
+    }
+
+    const ticket = await Ticket.findById(req.params.id);
+
+    // If ticket with the passed id does not exist.
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found!");
+    }
+
+    // Only the ticket owner can fetch their ticket.
+    if(ticket.user.toString() !== req.user.id) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("Not Authorized!");
+    }
+    
+    await Ticket.deleteOne(ticket);
+
+    res.status(200).json({ success: true });
+})
+
+// Description - Update a ticket
+// Route - PUT api/tickets/:id
+// Access - Private
+const updateTicket = asyncHandler(async (req, res) => {
+    // Get user from id in the JSON Web Token.
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("User not found!");
+    }
+
+    const ticket = await Ticket.findById(req.params.id);
+
+    // If ticket with the passed id does not exist.
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found!");
+    }
+
+    // Only the ticket owner can fetch their ticket.
+    if(ticket.user.toString() !== req.user.id) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("Not Authorized!");
+    }
+    
+    const updatedTicket = await Ticket.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        { new: true }
+    )
+
+    res.status(200).json(updatedTicket);
+})
+
 module.exports = {
     getTickets,
-    createTicket
+    getTicket,
+    createTicket,
+    deleteTicket,
+    updateTicket
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getTickets, createTicket} = require("../controllers/ticketController");
+const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = require("../controllers/ticketController");
 
 // Protect middleware.
 const { protectRoute } = require("../middleware/authMiddleware");
@@ -9,5 +9,11 @@ const { protectRoute } = require("../middleware/authMiddleware");
 // A user cannot get other user's tickets.
 // Also, user cannot create a ticket for some other user.
 router.route("/").get(protectRoute, getTickets).post(protectRoute, createTicket);
+
+// Route to get a single ticket.
+router.route("/:id")
+.get(protectRoute, getTicket)
+.delete(protectRoute, deleteTicket)
+.put(protectRoute, updateTicket);
 
 module.exports = router
