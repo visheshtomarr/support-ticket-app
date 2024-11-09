4cc95ac2bcf5f2c9d51ad79362cad709d6d5f673
visheshtomarr <visheshtomar7800@gmail.com>
2024-11-09 13:28:03 +0530

    Implemented delete/update note functionalities

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -62,7 +62,98 @@ const createNote = asyncHandler(async (req, res) => {
     res.status(200).json(note);
 })
 
+// Description - Delete a note
+// Route - DELETE api/tickets/:ticketId/notes/:noteId
+// Access - Private
+const deleteNote = asyncHandler(async (req, res) => {
+    // Get user from id in the JSON Web Token.
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("User not found!");
+    }
+
+    const ticket = await Ticket.findById(req.params.ticketId);
+
+    // If ticket with the passed id does not exist.
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found!");
+    }
+
+    // Only the ticket owner can fetch their ticket.
+    if(ticket.user.toString() !== req.user.id) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("Not Authorized!");
+    }
+
+    // Fetch note using noteId from req.params
+    const note = await Note.findById(req.params.noteId);
+
+    // Only the note associated with the ticket can be deleted.
+    if(!note || note.ticket.toString() !== ticket.id) {
+        // Note not found for the specified ticket
+        res.status(404);
+        throw new Error("Ticket does not have a note!");
+    }
+    
+    await note.deleteOne();
+
+    res.status(200).json({ success: true });
+})
+
+// Description - Update a note
+// Route - PUT api/tickets/:ticketId/notes/:noteId
+// Access - Private
+const updateNote = asyncHandler(async (req, res) => {
+    // Get user from id in the JSON Web Token.
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("User not found!");
+    }
+
+    const ticket = await Ticket.findById(req.params.ticketId);
+
+    // If ticket with the passed id does not exist.
+    if (!ticket) {
+        res.status(404);
+        throw new Error("Ticket not found!");
+    }
+
+    // Only the ticket owner can fetch their ticket.
+    if(ticket.user.toString() !== req.user.id) {
+        // Unauthorized user
+        res.status(401);
+        throw new Error("Not Authorized!");
+    }
+
+    // Fetch the note by noteId.
+    const note = await Note.findById(req.params.noteId);
+
+    // Check if the note exists and belongs to the specific ticket.
+    if (!note || note.ticket.toString() !== ticket.id) {
+        res.status(404);
+        throw new Error("Note not found for this ticket!");
+    }
+    
+    const updatedNote = await Note.findByIdAndUpdate(
+        req.params.noteId, 
+        req.body,
+        { new: true }
+    )
+
+    res.status(200).json(updatedNote);
+})
+
 module.exports = {
     getNotes,
     createNote,
+    deleteNote,
+    updateNote,
 }
--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { getNotes, createNote } = require("../controllers/noteController");
+const { getNotes, createNote, deleteNote, updateNote } = require("../controllers/noteController");
 
 // Protect route middleware.
 const { protectRoute } = require("../middleware/authMiddleware");
 
-// Create and protect our get route.
+// Create and protect our get and post routes.
 router.route("/").get(protectRoute, getNotes).post(protectRoute, createNote);
+
+// Create and protect our delete and put routes.
+router.route("/:noteId").delete(protectRoute, deleteNote).put(protectRoute, updateNote);
 
 module.exports = router
