e333003168f7e5b4d271f52c364a9590757d8542
visheshtomarr <visheshtomar7800@gmail.com>
2024-11-09 14:53:08 +0530

    Set up redux for ticket notes

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../features/auth/authSlice";
 import ticketReducer from "../features/ticket/ticketSlice";
+import noteReducer from "../features/notes/noteSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     ticket: ticketReducer,
+    note: noteReducer,
   },
 });
